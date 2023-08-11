eb14a65d036e67114d4cd5e6dd1bab34f2865b03
Emmanuel Duplay <emmanuel.duplay@gmail.com>
2023-08-11 13:19:33 -0400

    add wavelength calibration

--- code/Spectra/spectrum.py
+++ code/Spectra/spectrum.py
@@ -1,11 +1,13 @@
 import numpy as np
+from numpy.polynomial import polynomial
 import matplotlib.pyplot as plt
 
 class Spectrum:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        pixelcounts = np.loadtxt('rawdata/spectra/calibration/HgAr_pixel.csv', delimiter=',', skiprows=1)
+        self.wavelength_coeffs = polynomial.polyfit(pixelcounts[:,1], pixelcounts[:,0], 3)
 
-    def loadFromOceanView(self, path):
+    def loadFromOceanView(self, path, calibrate=True):
         self.metadata = {}
         with open(path, 'r') as f:
             preamble = list(f)[2:12]
@@ -21,7 +23,12 @@ class Spectrum:
                 self.metadata.update([(label, value)])
         self.rawmetadata = ''.join(preamble)
         self.data = np.loadtxt(path, skiprows=13)
-        self.wavelengths = self.data[:,0]
+        if calibrate:
+            self.wavelengths = polynomial.polyval(
+                np.arange(self.metadata['Number of Pixels in Spectrum']), 
+                self.wavelength_coeffs)
+        else:
+            self.wavelengths = self.data[:,0]
         self.counts = self.data[:,1]
 
     def plot(self, metadata=False):
@@ -36,7 +43,7 @@ class Spectrum:
 if __name__ == '__main__':
     s = Spectrum()
     # s.loadFromOceanView('rawdata/spectra/calibration/CalibrationHalogen.txt') 
-    # s.loadFromOceanView('rawdata/spectra/calibration/Mercury-Argog_Spectrum.txt') 
-    s.loadFromOceanView('rawdata/spectra/LSP52_S2_Subt2_15-13-04-066.txt') 
+    s.loadFromOceanView('rawdata/spectra/calibration/Mercury-Argog_Spectrum.txt') 
+    # s.loadFromOceanView('rawdata/spectra/LSP52_S2_Subt2_15-13-04-066.txt') 
     fig, ax = s.plot(metadata=True)
     plt.show()
